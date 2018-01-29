57984fe6fd2b4efea2aa7b6c5978246ce60d27c3
davhein <fr.heinemann.david@gmail.com>
2018-01-29 23:54:48 +0100

    v3

--- main.cpp
+++ main.cpp
@@ -38,25 +38,30 @@ char fromhex(char c)
     return c;
 }
 
-
+//ne fonctionne pas trop bien
 char *hexlify(char *str)
 {
     int l,i;
     char* t;
-    l = strlen(str)*2;
+    l = (strlen(str)+1);
     t = static_cast<char*>(malloc(l));
+
+    memset(t,0, sizeof(char));
     if ( &t  )
     {
-        for(i=0; i<l; i++)
+        for(i=0; i<=l; i++)
         {
-            sprintf(t+2*i, "%02x", str[i]);
+            sprintf(t+i, "%02x", str[i]);
+            if(i==l){
+                sprintf(t+i, NULL, str[i]);
+            }
         }
         return t;
     }
     return NULL;
 }
 
-
+//semble OK
 char *unhexlify(char *hstr)
 {
     int l, i; char *t; char c;
@@ -66,14 +71,14 @@ char *unhexlify(char *hstr)
     {
         for(i=0; i<l; i++)
         {
-            c = fromhex( hstr[2*i+1] ) + 16*fromhex( hstr[2*i] );
+            c = fromhex( hstr[i+1] ) + 16*fromhex( hstr[i] );
             t[i] = c;
         }
     }
     return t;
 }
 
-
+//semble ok sauf pour les insert, le insert renvoie systématiquement des exceptions.
 int connexion_mysql(string host,string user,string passwd,string database, int port,string query)
 {
 	cout << endl;
@@ -98,13 +103,13 @@ int connexion_mysql(string host,string user,string passwd,string database, int p
 	  delete stmt;
 	  delete con;
 	} catch (sql::SQLException &e) {
-	  	 cout << "# ERR: SQLException in " << __FILE__;
-		 cout << "(" << __FUNCTION__ << ") on line " ;
-		 cout << __LINE__ << endl;
-		 cout << "# ERR: " << e.what();
-		 cout << " (MySQL error code: " << e.getErrorCode();
-		 cout << ", SQLState: " << e.getSQLState() << " )" << endl;
-		 return EXIT_FAILURE;
+         cout << "# ERR: SQLException in " << __FILE__;
+         cout << "(" << __FUNCTION__ << ") on line " ;
+         cout << __LINE__ << endl;
+         cout << "# ERR: " << e.what();
+         cout << " (MySQL error code: " << e.getErrorCode();
+         cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+         return EXIT_FAILURE;
 	}
 
 	cout << endl;
@@ -135,7 +140,7 @@ string str_rand_generator(int len)
 void get_pkey(unsigned char* pkey)
 {
     string temp;
-    temp=sha256(str_rand_generator(rand()%32));
+    temp=sha256(str_rand_generator(32));
     std::copy(temp.begin(),temp.end(),pkey);
     pkey[temp.size()]='\0';
     //cout << "taille " << temp.size() << " " << temp << endl;
@@ -146,13 +151,13 @@ void gest_err(const char* message, void* data){
     cout << data << endl;
 }
 
-void get_pubkey(unsigned char* in_pkey,unsigned char* out_pubkey)
+void get_pubkey(unsigned char* in_pkey,unsigned char* out_pubkey,char* out_hexpubkey=0)
 {
     string res="";
     size_t taille=64;
     int32_t ecount = 0;
-    secp256k1_context *ctx;
     secp256k1_pubkey pubkey;
+    secp256k1_context *ctx;
     const char* message = "gest erreur - callback appelé";
     void* data;
     void (*counting_illegal_callback_fn)(const char*,void*) = gest_err;
@@ -164,36 +169,50 @@ void get_pubkey(unsigned char* in_pkey,unsigned char* out_pubkey)
         cout<< "ERROR : pubkey creation is fucken fucked, mate. BIG TIME"<<endl;
     }
 
-    //pubkey.data;
     secp256k1_ec_pubkey_serialize(ctx,out_pubkey,&taille,&pubkey,SECP256K1_EC_COMPRESSED);
-    cout << hexlify( out_pubkey) << endl;
+    /*if (&out_hexpubkey!=0){
+        out_hexpubkey=hexlify(static_cast<char*>(static_cast<void*>(out_pubkey)));
+    }*/
     secp256k1_context_destroy(ctx);
+    //cout << out_pubkey <<endl;
+    //cout << out_hexpubkey <<endl;
+    //cout << "pub_key : " << static_cast<char*>(static_cast<void*>(out_pubkey)) << endl;
 }
 
 int main()
 {
-    string host = "";
+    string host = "104.206.242.22";
     string user = "brt";
     string passwd = "Sepavrai#&!";
     string database = "rbt";
-    string query = "select address from incoming limit 10";
+    string txt_query = "";
     int port = 33666;
     int i;
     unsigned char pkey[65];//65
-    unsigned char pubkey[53];//53
+    unsigned char bin_pubkey[53];//53
+    char* bin_pkey;
+    char* hexpubkey;
     freopen("/home/david/output.txt","w",stdout);
-    for(i=0;i<10000;i++){
+    for(i=0;i<100;i++){
         get_pkey(pkey);
-        get_pubkey(pkey,pubkey);
-        cout << pkey << ";" << pubkey << std::endl;
-    }
-    return 0;
-    if(connexion_mysql(host,user,passwd,database,port,query)==EXIT_SUCCESS){
-	    cout<< "Successfull connection and querying";
-    }else{
-	    cout<< "ERROR: Connexion unsuccessfull";
-    }
+        get_pubkey(pkey,bin_pubkey,hexpubkey);
+        cout << i << endl;
+        cout << pkey << endl;
 
+        bin_pkey = unhexlify(static_cast<char*>(static_cast<void*>(pkey)));
+        txt_query = "insert into incoming(address,private) values('";
+        txt_query += reinterpret_cast<const char*>(bin_pubkey);
+        txt_query += "','";
+        txt_query += bin_pkey;
+        txt_query += "')";
+
+        cout << txt_query << endl;
+        if(connexion_mysql(host,user,passwd,database,port,txt_query)==EXIT_SUCCESS){
+            cout<< "Successfull connection and querying"<<endl;
+        }else{
+            cout<< "ERROR: Connexion unsuccessfull"<<endl;
+        }
+        cout << "--------------------------------------------" << endl;
+    }
     return 0;
 }
-
